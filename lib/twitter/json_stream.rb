ca701b89500ea176b97e28d1e2ab67699f6c1bc3
Kouhei Sutou <kou@clear-code.com>
2011-06-01 00:32:49 +0900

    [oauth] use not escaped param and key pairs for OAuth header.

--- lib/twitter/json_stream.rb
+++ lib/twitter/json_stream.rb
@@ -311,13 +311,13 @@ module Twitter
       @options[:params].merge( :track => @options[:filters] ).each do |param, val|
         next if val.empty?
         val = val.join(",") if val.respond_to?(:join)
-        flat[escape(param)] = escape(val)
+        flat[param.to_s] = val.to_s
       end
       flat
     end
 
     def query
-      params.map{|pair| pair.join("=")}.sort.join("&")
+      params.map{|param, value| [escape(param), escape(value)].join("=")}.sort.join("&")
     end
 
     def escape str
